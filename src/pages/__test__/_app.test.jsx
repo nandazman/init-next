100a045a56c00b784ef9d8595b5c4b994f8d1f29
nandazman <nandazmann@gmail.com>
2022-06-20 14:56:13 +0700

    fix testing _app

--- src/pages/__test__/_app.test.jsx
+++ src/pages/__test__/_app.test.jsx
@@ -2,9 +2,16 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import App from "../_app";
 
+const test = () => (
+  <div
+    data-testid="test"
+  />
+);
+
+
 describe("Page _app", () => {
   it("should render the page", () => {
-    render(<App Component={<div data-testid="page-app"></div>} />);
-    expect(screen.getByTestId("page-app")).toBeInTheDocument();
+    render(<App Component={test} pageProps={{}} />);
+    expect(screen.getByTestId("test")).toBeInTheDocument();
   });
 });
