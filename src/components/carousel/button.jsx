77f3e9ebbf7d2b3b6f13f3c1683e2069f3dce0fb
nandazman <nandazmann@gmail.com>
2022-06-20 12:58:49 +0700

    add jest to danger file

--- src/components/carousel/button.jsx
+++ src/components/carousel/button.jsx
@@ -1,28 +1,8 @@
-export const DotButton = ({ selected, onClick }) => (
+export const DotButton = ({ onClick }) => (
   <button
     data-testid="carousel-dot-button"
-    className={`embla__dot ${selected ? "is-selected" : ""}`}
+    className="embla__dot"
     type="button"
     onClick={onClick}
   />
-);
-
-export const PrevButton = ({ enabled, onClick }) => (
-  <button
-    className="embla__button embla__button--prev"
-    onClick={onClick}
-    disabled={!enabled}
-  >
-    Prev
-  </button>
-);
-
-export const NextButton = ({ enabled, onClick }) => (
-  <button
-    className="embla__button embla__button--next"
-    onClick={onClick}
-    disabled={!enabled}
-  >
-    Next
-  </button>
 );
