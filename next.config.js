25643c073f070ee19b15c0daa134e630deaf0e5d
nandazman <nandazmann@gmail.com>
2022-06-20 10:56:47 +0700

    install tailwind scss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
   reactStrictMode: true,
-}
+  images: {
+    domains: ["storage.googleapis.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
