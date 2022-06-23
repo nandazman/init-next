a20cc0b789b353474c5c1c42b39e8a913e89bef6
nandazman <nandazmann@gmail.com>
2022-06-23 16:23:20 +0700

    add admin page

---
+++ src/pages/admin/index.jsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/Button/Button";
+import { ButtonPurple } from "@/components/Button/ButtonPurple";
+import { Modal } from "@/components/Modal/Modal";
+
+export default function Admin() {
+  return (
+    <div data-testid="page-admin-dashboard">
+      <h1 className="text-identity">H1 Hello world</h1>
+      <h2>H2 LALALA</h2>
+      <p className="text-button bg-green">HMMMM</p>
+      <ButtonPurple>haha</ButtonPurple>
+      <Button>haha</Button>
+      <Modal show>
+        <h1 className="text-identity">HALO HAHA</h1>
+      </Modal>
+    </div>
+  );
+}
