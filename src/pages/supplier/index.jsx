0e6776e2b29bc5cecc57c3e8c850931271433968
nandazman <nandazmann@gmail.com>
2022-06-24 12:47:01 +0700

    add supplier page

---
+++ src/pages/supplier/index.jsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function supplierIndex() {
+  return (
+    <>
+      <Head>
+        <title>Supplier Page | Dashboard</title>
+        <meta name="description" content="Dashboard untuk supplier" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
