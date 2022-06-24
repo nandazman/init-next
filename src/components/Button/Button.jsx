5286cda8733318536e24d41fb83b428ad12677b6
nandazman <nandazmann@gmail.com>
2022-06-24 14:30:54 +0700

    update variant for button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -7,14 +7,17 @@ export const Button = (props) => {
     type = "button",
     children,
     width = "100px",
+    variant,
     ...restProps
   } = props;
+
+  const btnVariant = variant ? `btn-${variant}` : ""
   return (
     <button
       data-testid="component-button"
       {...restProps}
       style={{ width }}
-      className={cn("p-10px rounded-[5px] text-button", className)}
+      className={cn("p-10px rounded-[5px] text-button", className, btnVariant)}
       type={type}
       onClick={onClick}
     >
